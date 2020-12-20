61160d40c890c3d6b5989e3c4b06141f5d1fe4ba
Nsingletary <singletarybush@gmail.com>
2020-12-20 14:36:56 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,6 @@
 class Dog
-    attr_accessor :name
-  
     @@all = []
+    attr_accessor :name
   
     def initialize(name)
       @name = name
@@ -9,10 +8,9 @@ class Dog
     end
   
     def self.all
-
-    @@all.each do |dog|
+      @@all.each do |dog|
         puts dog.name
-    end
+      end
     end
   
     def self.clear_all
