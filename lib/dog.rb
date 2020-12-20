61f676cfcecd0f13508dc395f4b683835b46ec6a
Nsingletary <singletarybush@gmail.com>
2020-12-20 14:26:56 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,20 @@
 class Dog
-  attr_accessor :name
-
-  @@all = []
-
-  def initialize(name)
-    @name = name
-    @@all << self
-  end
-
-  def self.all
-    @@all.each do |dog|
-      puts dog.name
+    attr_accessor :name
+  
+    @@all = []
+  
+    def initialize(name)
+      @name = name
+      @@all << self
     end
-
-  def self.clear_all
-    @@all.clear
+  
+    def self.all
+      @@all.each do |dog|
+        puts dog.name
+      end
+    end
+  
+    def self.clear_all
+      @@all.clear
+    end
   end
-end
